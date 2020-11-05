e5c2f1775913681ca302e70e0d4d6e4974dd02f8
Vlad <vlad.nifos@gmail.com>
2020-11-05 17:41:18 +0300

    Get information about promo (route)

---
+++ src/pages/api/promocode/index.ts
@@ -0,0 +1,30 @@
+import { NextApiResponse } from 'next';
+import { NextApiRequestWithUser } from '../../../libs/withUser';
+import { discountModel } from '../../../models/discount';
+
+/**
+ * Get information about promocode
+ */
+async function handler(req: NextApiRequestWithUser, res: NextApiResponse) {
+  try {
+    const { promocode } = req.body;
+    const discountInformation = await discountModel.getDiscountByPromocode(
+      promocode
+    );
+    return res.json({
+      discount: {
+        percentage: discountInformation[0]?.percent_discount,
+        title: discountInformation[0]?.title,
+        description: discountInformation[0]?.description,
+        dateTo: discountInformation[0]?.date_to,
+      },
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: true,
+      message: error.toString(),
+    });
+  }
+}
+
+export default handler;
