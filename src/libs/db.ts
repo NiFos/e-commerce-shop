51f0855b3744b5651e71ab918b60345c07d64845
Vlad <vlad.nifos@gmail.com>
2020-12-07 19:52:44 +0300

    heroku: db url

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -1,6 +1,6 @@
 import knex from 'knex';
 
-const connectionString = process.env.PG_CONNECTION_STRING || '';
+const connectionString = process.env.DATABASE_URL || '';
 let cachedConnection: knex;
 
 /**
